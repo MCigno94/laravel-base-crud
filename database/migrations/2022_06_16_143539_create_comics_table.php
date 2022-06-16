5343e07cd416d4cd9855c0b00f9ee23967f501c0
Mattia Cigno <geom.mattiacigno@gmail.com>
2022-06-16 16:38:37 +0200

    problema con migration

--- database/migrations/2022_06_16_143539_create_comics_table.php
+++ database/migrations/2022_06_16_143539_create_comics_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class LaravelComicsCrud extends Migration
+class CreateComicsTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
+    
     public function up()
     {
         Schema::create('comics', function (Blueprint $table) {
